f9acf36bafc1577121d1f86f54f396f270612b7f
sylvey <a.few.star@gmail.com>
2022-10-06 11:43:45 +0800

    codegen

---
+++ codegen/component.py
@@ -0,0 +1,24 @@
+import sys
+
+print (sys.argv[1:])
+
+name = sys.argv[1]
+path = sys.argv[2] + '/'
+
+
+fo = open(path + name + '.js', "w")
+str = "test"
+fo.write('import React , { useState, useEffect } from "react";\n')
+fo.write('import { StyleSheet, Text, View } from "react-native";\n\n')
+fo.write('const %s = () => {\n'%(name))
+fo.write('  return (\n')
+fo.write('    <View>\n')
+fo.write('      <Text>Home</Text>\n')
+fo.write('    </View>\n')
+fo.write('  );\n')
+fo.write('};\n\n')
+fo.write('export default %s;'%(name))
+
+fo.close()
+
+
