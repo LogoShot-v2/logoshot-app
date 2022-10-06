31bc7f2e17eb432635f5b0d05de0cd41f0451454
sylvey <a.few.star@gmail.com>
2022-10-06 22:00:57 +0800

    login frontend finished

--- pages/Login.js
+++ pages/Login.js
@@ -1,12 +1,47 @@
-import React , { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Alert } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import * as Facebook from "expo-facebook";
 
 const Login = () => {
+  const facebooklogin = async () => {
+    try {
+      await Facebook.initializeAsync({
+        appId: "848189786592365",
+      });
+      const { type, token, expirationDate, permissions, declinedPermissions } =
+        await Facebook.logInWithReadPermissionsAsync({
+          permissions: ["public_profile"],
+        });
+      if (type === "success") {
+        // Get the user's name using Facebook's Graph API
+        const response = await fetch(
+          `https://graph.facebook.com/me?access_token=${token}`
+        );
+        Alert.alert("Logged in!", `Hi ${(await response.json()).name}!`);
+      } else {
+        // type === 'cancel'
+      }
+    } catch ({ message }) {
+      alert(`Facebook Login Error: ${message}`);
+    }
+  };
+
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <TouchableOpacity onPress={facebooklogin}>
+        <Text>login</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default Login;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
