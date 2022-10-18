30cd7a843aa1d54396814ba1e2ae6111ba77dc60
sylvey <a.few.star@gmail.com>
2022-10-18 16:51:55 +0800

    SignIn接上後端

--- pages/Login.js
+++ pages/Login.js
@@ -3,9 +3,14 @@ import { StyleSheet, Text, View, Alert } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import * as Facebook from "expo-facebook";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { LoginToFireBase } from "../axios/api";
+import { LoginToFireBase, SignInToFireBase } from "../axios/api";
+import LgsTextInput from "../components/lgsTextInput";
+import { Background, Scroll, ContentContainer } from "../components/lgsScreen";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const facebooklogin = async () => {
     try {
       await Facebook.initializeAsync({
@@ -19,9 +24,7 @@ const Login = () => {
         const response = await fetch(
           `https://graph.facebook.com/me?access_token=${token}&fields=id,name,picture.height(500)`
         );
-
         const { id, name, picture } = await response.json();
-
         await AsyncStorage.setItem(
           "@userInfo",
           JSON.stringify({
@@ -29,9 +32,12 @@ const Login = () => {
             name: name,
             token: token,
             image: picture,
+            accountType: "Facebook",
           })
         );
-        Alert.alert("Logged in!", `Hi ${(await response.json()).name}!`);
+        Alert.alert("Logged in!", `Hi ${name}!`);
+        const localData = await AsyncStorage.getItem("@userInfo");
+        console.log("localData", localData);
       } else {
         // type === 'cancel'
       }
@@ -39,25 +45,72 @@ const Login = () => {
       alert(`Facebook Login Error: ${message}`);
     }
   };
-
   const firebaselogin = async () => {
     try {
-      await LoginToFireBase("sylvia.grey.lin@gmail.com", "06745sylvia");
+      const loginDatas = await LoginToFireBase(email, password);
+      console.log("hi", loginDatas);
+      await AsyncStorage.setItem(
+        "@userInfo",
+        JSON.stringify({
+          userId: loginDatas["res"]["userId"],
+          name: email,
+          accountType: "FireBase",
+        })
+      );
+
+      const localData = await AsyncStorage.getItem("@userInfo");
+      console.log("localData", localData);
+      Alert.alert("Logged in!");
     } catch (e) {
       console.log(e.data);
       alert(e.data);
     }
   };
 
+  const signIn = async () => {
+    const signInStatus = await SignInToFireBase(email, password);
+    console.log(signInStatus);
+    Alert.alert(
+      `驗證信已寄至${signInStatus}，請至信箱中點擊連結完成驗證。（請小心，驗證信有可能會被信箱中被歸類為垃圾信件）`
+    );
+  };
+
+  const logout = async () => {
+    await AsyncStorage.clear();
+    console.log("logout clear:", await AsyncStorage.getAllKeys());
+  };
+
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={() => facebooklogin()}>
-        <Text>FaceBooklogin</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => firebaselogin()}>
-        <Text>login</Text>
-      </TouchableOpacity>
-    </View>
+    <Background>
+      <Scroll>
+        <ContentContainer>
+          <LgsTextInput
+            style={styles.input}
+            placeholder={"請輸入電子郵件"}
+            value={email}
+            onChangeText={setEmail}
+          />
+          <LgsTextInput
+            style={styles.input}
+            placeholder={"請輸入密碼"}
+            value={password}
+            onChangeText={setPassword}
+          />
+          <TouchableOpacity onPress={() => facebooklogin()}>
+            <Text>FaceBooklogin</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => firebaselogin()}>
+            <Text>login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => logout()}>
+            <Text>logout</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => signIn()}>
+            <Text>signIn</Text>
+          </TouchableOpacity>
+        </ContentContainer>
+      </Scroll>
+    </Background>
   );
 };
 
@@ -68,5 +121,9 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  input: {
+    marginTop: 10,
+    borderRadius: 8,
   },
 });
