71d4fcc632c8b3020d9daa80432132e90e871c2e
sylvey <a.few.star@gmail.com>
2022-10-21 14:53:02 +0800

    codegen頁

--- codegen/component.py
+++ codegen/component.py
@@ -9,12 +9,23 @@ path = sys.argv[2] + '/'
 fo = open(path + name + '.js', "w")
 str = "test"
 fo.write('import React , { useState, useEffect } from "react";\n')
+fo.write('''import {
+  Background,
+  Scroll,
+  ContentContainer,
+  ListBlock,
+} from "../components/lgsScreen"; \n''')
+fo.write('import { FONTS } from "../constant";')
 fo.write('import { StyleSheet, Text, View } from "react-native";\n\n')
 fo.write('const %s = () => {\n'%(name))
 fo.write('  return (\n')
-fo.write('    <View>\n')
-fo.write('      <Text>%s</Text>\n'%(name))
-fo.write('    </View>\n')
+fo.write('    <Background>\n')
+fo.write('    <Scroll>\n')
+fo.write('    <ContentContainer>\n')
+fo.write('      <Text style={ FONTS.h1 }>%s</Text>\n'%(name))
+fo.write('    </ContentContainer>\n')
+fo.write('    </Scroll>\n')
+fo.write('    </Background>\n')
 fo.write('  );\n')
 fo.write('};\n\n')
 fo.write('export default %s;'%(name))
