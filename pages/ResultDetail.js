aa408c337329b938ccdfa3131819ea8dd85153f2
viola50420 <92725965+viola50420@users.noreply.github.com>
2022-11-19 19:25:27 +0800

    commit

--- pages/ResultDetail.js
+++ pages/ResultDetail.js
@@ -1,27 +1,47 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, Image, FlatList, StatusBar, TouchableOpacity} from "react-native";
+import { StyleSheet, Text, View, Image, FlatList, StatusBar, TouchableOpacity, Dimensions} from "react-native";
+import { Button } from 'react-native-elements';
 import { Table, Row, Rows, Col, Cols, TableWrapper} from "react-native-table-component";
 import { Background, Scroll, ContentContainer } from "../components/lgsScreen";
 import Carousel from 'react-native-snap-carousel';
-import { SearchText } from "../axios/api"
+import { PostAddFavoriteFile, SearchText } from "../axios/api"
 import { icons, COLORS, FONTS, SIZES, classCodeList } from "../constant";
+import LgsCarousel from "../components/carousel";
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-
-
+const imageArr =  ["tmark-image-url_1",  "tmark-image-url_2",  "tmark-image-url_3",  "tmark-image-url_4",  "tmark-image-url_5"]
+const CarouselCardItem = ({item, index }) => {
+    
+    return (
+      <View  key={index}>
+        <Image
+          source={{ uri:  "http://140.112.106.88:8082/"
+          + params.trademarkDetail["_source"][imageArr[index]] }}
+        
+        />
+      </View>
+    );
+  };
 
 const ResultDetail = ({ navigation: { navigate }, route: { params } }) => {
-
+    
+    
     useEffect(() => {  
-  
+
 
           console.log("upup")
           
-            
-          console.log(params.trademarkDetail)
+          console.log( params.trademarkDetail["_source"][imageArr[0]])
+        //   console.log(params.trademarkDetail)
     
           console.log("d") 
 
     }, [params]);
+    const AddFavorite = async () => {
+      
+      //加到我的最愛
+      };
+    
 
     return (
 
@@ -37,31 +57,12 @@ const ResultDetail = ({ navigation: { navigate }, route: { params } }) => {
 
                      }}
                    style={{ resizeMode: "contain", width: "100%", height: "100%", borderWidth:2}} />
+
+                   {/* <LgsCarousel  
+                
+                    renderItem={CarouselCardItem}/> */}
                     </View>
                     <View style={{ margintop:50, backgroundColor: 'FFFFFF' }}>
-
-                        {/* <Table style={{ backgroundColor: 'FFFFFF' }} borderStyle={{ borderWidth: 1, borderColor: '#1E1F20' }}>
-                        <TableWrapper>
-
-                        
-                        <Row flexArr={[1.2, 2]} data={[" 申請案號", params.trademarkDetail["_source"]["appl-no"]]} textStyle={{ marginLeft: 4 }} />    
-                        <Row flexArr={[1.2, 2]} data={[" 商標名稱", params.trademarkDetail["_source"]["tmark-name"]]} textStyle={{ marginLeft: 4 }} />
-                        <Row flexArr={[1.2, 2]} data={[" 申請日期", params.trademarkDetail["_source"]["appl-date"]]} textStyle={{ marginLeft: 4 }} />
-                        </TableWrapper> 
-                            <TableWrapper  style={{flexDirection: 'row'}}>
-                            <Row data={["申請人"]} style={{}}/>
-                            <Row data={["中文名稱", params.trademarkDetail["_source"]["appl-date"]]} textStyle={{ marginLeft: 4 }}/>
-                            <Row data={["地址", params.trademarkDetail["_source"]["applicant-address"]]} textStyle={{ marginLeft: 4 }}/>
-                            <Row data={["國籍", params.trademarkDetail["_source"]["applicant-chinese-country-nam"]]} textStyle={{ marginLeft: 4 }}/>
-                            
-                            {/* <Row data={[" 商標地址", params.trademarkDetail["_source"]["angents-address"]]} textStyle={{ marginLeft: 4 }}/>
-                            <Row data={[" 商標地址", params.trademarkDetail["_source"]["angents-address"]]} textStyle={{ marginLeft: 4 }}/> 
-
-                             </TableWrapper>
-                    
-                   
-                             
-                        </Table> */}
                         
                         <View style = {{marginLeft: 30, marginRight: 30,}}>
                         <Text style = {{  fontSize: 20, margin: 5 , textDecorationLine:'underline',}}>商標 </Text>
@@ -76,10 +77,40 @@ const ResultDetail = ({ navigation: { navigate }, route: { params } }) => {
                         <Text style = {{   ...FONTS.h3,  margin: 3,}}>國籍 ： {params.trademarkDetail["_source"]["applicant-chinese-country-name"]}</Text>
                         
                         </View>
+                        <View style = {{margin: 20,marginLeft:20, flexDirection: 'row',  alignItems: "center", }}> 
+                          
+                          <Button
+                     
+                             type="clear"
+                             icon={
+                                 <Icon
+                                 name="home"
+                                 size={22}
+                                 color="#606d87" />
+                                  }
+                             onPress={() => navigate("Home")}
+                         />
+                             
+                          <Button
+                     
+                         type="clear"
+                         icon={
+                         <Icon
+                               name="heart-o"
+                           size={20}
+                           color="red"/>
+                       } 
+                       onPress={() => AddFavorite()}
+                       />
 
+                       </View>
+                       
+               
                     </View>
+               
                 
                 </View>
+
             </Scroll>
         </Background>
 
